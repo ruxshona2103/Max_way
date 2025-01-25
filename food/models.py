ca50f74fed45132a05295145490f6ef4182eefed
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-01-25 18:20:08 +0500

    Food directory updated

--- food/models.py
+++ food/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=300, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=300, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    cost = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="products")
+
+    def __str__(self):
+        return self.title
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    phone_number = models.IntegerField(max_length=13)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+
+class Order(models.Model):
+    payment_type = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(null=False, blank=True, default=1)
+    address = models.CharField(null=False, blank=False, max_length=300)
+    customer = models.ForeignKey(Customer, max_length=250, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+
